1e26789142f8cc2d1749d0e38813dd101ce3a19e
tancia.joy <tancia20@gmail.com>
2023-11-24 14:49:08 +0530

    added linkedin processing

--- ice_breaker.py
+++ ice_breaker.py
@@ -32,22 +32,38 @@
 from langchain.prompts import PromptTemplate
 from langchain.chat_models import ChatOpenAI
 from langchain.chains import LLMChain
+
+from output_parsers import person_intel_parser
 from third_parties.linkedin import scrape_linkedin_profile
 from agents.linkedin_lookup_agent import lookup as linkedin_lookup_agent
 
 
-if __name__ == "__main__":
-    print("Hello LangChain")
+def ice_break(name: str) -> str:
     linkedin_profile_url = linkedin_lookup_agent(name="Harrison Chase")
+    linkedin_data = scrape_linkedin_profile(linkedin_profile_url=linkedin_profile_url)
     summary_template = """
-        Given the Linkedin information {information} about a person from which I want you to create:
-        1. a short summary
-        2. two interesting facts about them
-        """
+            Given the Linkedin information {information} about a person from which I want you to create:
+            1. a short summary
+            2. two interesting facts about them
+            3. A topic that may interest them
+            4. 2 creative icebreakers to open a conversation with them
+                    \n{format_instructions}
+            """
     summary_prompt_template = PromptTemplate(
-        input_variables=["information"], template=summary_template
+        input_variables=["information"],
+        template=summary_template,
+        partial_variables={
+            "format_instructions": person_intel_parser.get_format_instructions()
+        },
     )
     llm = ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo")
     chain = LLMChain(llm=llm, prompt=summary_prompt_template)
     linkedin_data = scrape_linkedin_profile(linkedin_profile_url=linkedin_profile_url)
-    print(chain.run(information=linkedin_data))
+    result = chain.run(information=linkedin_data)
+    print(result)
+    return result
+
+
+if __name__ == "__main__":
+    print("Hello LangChain")
+    ice_break(name="Harrison Chase")
